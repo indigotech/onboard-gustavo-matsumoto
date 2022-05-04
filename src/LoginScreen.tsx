9312ee613a1215ca40ec16305a8dd295893c98c3
Gustavo Matsumoto <gustavo.matsumoto@taqtile.com.br>
2022-05-04 11:33:47 -0300

    bugfix

---
+++ src/LoginScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function loginScreen() {
+  return (
+    <>
+      <h1>Bem-vindo(a) à Taqtile!</h1>
+      <div>
+        <text>E-mail</text>
+        <input />
+      </div>
+      <div>
+        <text>Senha</text>
+        <input />
+      </div>
+    </>
+  );
+}
+
+export default loginScreen
